ccc317840a50edcdbbf7caa82f07893b5a62e464
tomekszub <szuberttomek0@gmail.com>
2024-01-18 23:27:59 +0100

    changed song count multiplier (custom ranking formula)

--- Assets/Scripts/BinaryRanker/RankingTab.cs
+++ Assets/Scripts/BinaryRanker/RankingTab.cs
@@ -85,7 +85,7 @@ namespace Immortus.SongRanker
                     Debug.LogError($"Artist with id {kvp.Key} deos not exist or is null!");
                     continue;
                 }
-                _artistRanking.Add((artist, ratedSongs, ratingSum / ratedSongs, (ratingSum / ratedSongs) + ((ratedSongs - 1) * 5)));
+                _artistRanking.Add((artist, ratedSongs, ratingSum / ratedSongs, (ratingSum / ratedSongs) + ((ratedSongs - 1) * 4)));
             }
 
             _rankingDirtyLists.Remove(ARTISTS);
